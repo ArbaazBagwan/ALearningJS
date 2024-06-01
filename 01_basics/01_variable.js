af2e60db86d45ca05579e0852cac45c69bf81400
ArbaazBagwan <111445181+ArbaazBagwan@users.noreply.github.com>
2024-06-01 16:30:15 +0000

    variable_file

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountID = 122                   //cannot be changed
+let accountName = "BlackShout"          //to be used
+var accountEmail = "Black@gmail.com"    //not to use as it does not has block scope
+accountCity = "Nashik"                  //variable can be declared like this but not recommended
+let accountState;                       //value will be undefined
+
+console.table([accountID, accountName, accountEmail, accountCity, accountState]);
